ef3df93d8ceb1c9099337008f69f55ae921e8fa7
HyrumDickinson <hyrumsd2@illinois.edu>
2021-12-27 12:04:54 -0600

    renamed file

---
+++ src/main/java/tictactoe/intelligence.java
@@ -0,0 +1,19 @@
+public class Intelligence {
+
+    // evaluation helper functions
+    public static
+
+    // function that evaluates a position
+    public static int evaluation(int[9] board) {
+        assert (board.length == 9) : "board length must be 9";
+        
+        // evaluation functions
+
+
+        return eval; // positive if first player is winning
+    }
+
+    // minimax algorithm that uses previous function recursively
+    // alpha-beta algorithm inside above function once it works well
+
+}
