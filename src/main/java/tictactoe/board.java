62ab47d85f97ec8acc0f4a7e66de4f26d72c4da8
HyrumDickinson <hyrumsd2@illinois.edu>
2021-12-27 12:05:15 -0600

    finished prototype

--- src/main/java/tictactoe/board.java
+++ src/main/java/tictactoe/board.java
@@ -1,36 +1,32 @@
 public class Position {
 
     public int[] board = new int[9];
-    public int turn;
-    public int evaluation;
 
     Position(int topLeft,    int topCenter,    int topRight, 
              int middleLeft, int middleCenter, int middleRight, 
              int bottomLeft, int bottomCenter, int bottomRight) {
         this.board = [topLeft, topCenter, topRight, middleLeft, middleCenter, middleRight, bottomLeft, bottomCenter, bottomRight];
     }
-    public int[] setBoard() {
-
+    public int[] setBoard(int[9] input) {
+        this.board = input;
     }
     public int[] getBoard() {
         return this.board;
     }
-    public int getTurn() {
-        this.computeTurn();
-        return this.turn;
-    }
-    public int getEvaluation() {
-        this.computeEvaluation();
-        return this.evaluation;
-    }
 
-    private int computeTurn(this.board) {
-        ;
-        return this.turn;
+    private int turn(this.board) { // returns the player who is to move in the current position
+        int index = 0;
+        for (int space : board) {
+            index += space;
+        }
+        if (index == 0) { // equal numbers of pieces on the board; it's first player's move
+            return 1
+        } else {
+            return -1
+        }
     }
-    private int computeEvaluation(this.board) {
-        ;
-        return this.evaluation;
+    private int evaluation(this.board) {
+        return Intelligence.evaluation(this.board);
     }
 
 }
