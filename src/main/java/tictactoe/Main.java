a8204b95fe5b348dded32203c7a5f595b9292454
HyrumDickinson <hyrumsd2@illinois.edu>
2021-12-28 14:33:50 -0600

    added basic structure

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -1,3 +1,14 @@
 // this will contain the code executed when the program is ran
 // prompts allowing the user to choose game modes, action flow, etc.
 // this will function as a script that directs the functionality of the other files
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to tictactoe_java");
+
+        Game newGame = new Game();
+        
+    }
+}
