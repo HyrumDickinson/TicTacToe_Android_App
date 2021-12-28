9d7431972d1773cd2f4e24175c5da59f054055b0
HyrumDickinson <hyrumsd2@illinois.edu>
2021-12-28 11:13:17 -0600

    added game features

--- src/main/java/tictactoe/play.java
+++ src/main/java/tictactoe/play.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 // this is the class for a single game
 // intelligence algorithms will analyze positions
 // this will have some extra info
@@ -5,4 +9,17 @@
     // going back and forth between players
     // enforcing rules
     // will contain the functions for random, intelligent, and pvp modes
-    
+
+public class Play {
+    // state
+        // changelog contains order of moves, who made each move inferred by order, and location of moves
+        // list would be easiest
+    public List<Integer> changeLog = new ArrayList<>();
+    // int of how many times a player's undone a move
+    public int takebackX = 0; // x goes first
+    public int takebackO = 0;
+    public String playernameX; // until initialized, is empty ""
+    public String playername0;
+
+
+}
