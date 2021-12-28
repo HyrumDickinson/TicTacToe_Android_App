8b05a8e7dbd05a2c17d4cbf8e0dbd105523ee4bd
HyrumDickinson <hyrumsd2@illinois.edu>
2021-12-28 09:50:29 -0600

    renamed file

---
+++ src/main/java/tictactoe/Main.java
@@ -0,0 +1,3 @@
+// this will contain the code executed when the program is ran
+// prompts allowing the user to choose game modes, action flow, etc.
+// this will function as a script that directs the functionality of the other files
