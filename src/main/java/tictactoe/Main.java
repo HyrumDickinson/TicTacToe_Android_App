e93dba9efd324336fafc89188b57064605d7bcb9
HyrumDickinson <hyrumsd2@illinois.edu>
2021-12-29 14:33:56 -0600

    beginning to end gameplay

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -6,9 +6,31 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to tictactoe_java");
 
+        // board setup
+        System.out.println("Welcome to tictactoe_java");
         Game newGame = new Game();
         
+        // player setup
+        newGame.scanPlayerName(true);
+        newGame.scanPlayerName(false);
+        System.out.println(newGame.getPlayerName(true) + " vs " + newGame.getPlayerName(false));
+
+        // gameplay
+        while (newGame.getChangeLog().size() < 9 && Intelligence.checkWin(newGame.getBoard()) == 0) {
+            newGame.playRandom();
+            newGame.positionIsValid();
+        }
+
+        // end of game
+        if (Intelligence.checkWin(newGame.getBoard()) == 0) {
+            System.out.println("Congratulations both players! This game is a tie");
+        } else if (Intelligence.checkWin(newGame.getBoard()) == 1) {
+            System.out.println("Congratulations " + newGame.getPlayerName(true) + " for winning this game!");
+        } else if (Intelligence.checkWin(newGame.getBoard()) == -1) {
+            System.out.println("Congratulations " + newGame.getPlayerName(false) + " for winning this game!");
+        } else {
+            System.out.println("End of game error");
+        }
     }
 }
