499d490c557a860e99dfa39945c2810f1b6bab6a
HyrumDickinson <hyrumsd2@illinois.edu>
2021-12-29 14:34:22 -0600

    restructured state

--- src/main/java/tictactoe/Game.java
+++ src/main/java/tictactoe/Game.java
@@ -1,67 +1,107 @@
+// this class represents a single game
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-
-// this is the class for a single game
-// intelligence algorithms will analyze positions
-// this will have some extra info
-    // keeping track of previous moves
-    // going back and forth between players
-    // enforcing rules
-    // will contain the functions for random, intelligent, and pvp modes
+import java.util.Random;
 
 public class Game extends Position {
-    // state
-        // changelog contains order of moves, who made each move inferred by order, and location of moves
-        // list would be easiest
-    public List<Integer> changeLog = new ArrayList<>(); // begins empty
-    // int of how many times a player's undone a move
-    public int takeBackX = 0; // x goes first
-    public int takeBackO = 0;
-    public String playerNameX; // until initialized, is empty ""
-    public String playerName0;
+
+    // STATE
+    // changelog contains order of moves, who made each move inferred by odd/even in order, and location of moves being the actual values contained
+    private List<Integer> changeLog = new ArrayList<>(); // begins empty
+    // takebacks are ints counting how many times each player's undone a move
+    private int[] takeBack = new int[2]; // x is first
+    private String[] playerName = new String[2]; // until initialized, is empty "" - x is first
     
-    Game(int[] setboard) {
+    // CONSTRUCTORS
+    public Game(int[] setboard) {
         super(setboard); // new Game instance should have all data from Position and state above
-        int[] newBoard = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-        this.setBoard(newBoard);
+    }
+    public Game() {
+        super();
     }
 
+    // SETTERS AND GETTERS
+    public void setChangeLog(List<Integer> input) {
+        changeLog = input;
+    }
+    public List<Integer> getChangeLog() {
+        return changeLog;
+    }
+    public void setTakeBack(boolean isPlayerX) {
+        if (isPlayerX) {
+            takeBack[0]++;
+        } else {
+            takeBack[1]++;
+        }
+    }
+    public void setPlayerName(boolean isPlayerX, String name) {
+        if (isPlayerX) {
+            playerName[0] = name;
+        } else {
+            playerName[1] = name;
+        }
+    }
+    public int getTakeBack(boolean isPlayerX) {
+        if (isPlayerX) {
+            return takeBack[0];
+        } else {
+            return takeBack[1];
+        }
+    }
+    public String getPlayerName(boolean isPlayerX) {
+        if (isPlayerX) {
+            return playerName[0];
+        } else {
+            return playerName[1];
+        }
+    }
+    // METHODS
     public void setPiece(int location) {
-        // ! exception for incorrect piece placement here
-        this.board[location] = this.turn();
+        if ((location < -1) || (location > 9)) {
+            throw new IllegalArgumentException("location is not on the board");
+          }
+        if (this.getBoard()[location] != 0) {
+            throw new IllegalArgumentException("location is already occupied");
+        }
+        this.getBoard()[location] = this.turn();
     }
 
-    public boolean isValid() {
+    public boolean positionIsValid() {
         int index = 0; // checks if X and O have a valid number of squares on the board
-        for (int squares : this.board) {
+        for (int squares : this.getBoard()) {
             index += squares;
         }
         if (index != 0 && index != 1) {
             return false;
         }
-        if (this.board.length != 9) { // checks if board is correct size
+        if (this.getBoard().length != 9) { // checks if board is correct size
             return false;
         }
         return true;
     }
 
-    public void setPlayerNameX() {
-        try (Scanner playerNameScannerX = new Scanner(System.in)) {
-            System.out.println("Player 1 enter name");
-
-            String playerNameX = playerNameScannerX.nextLine();  // Read user input
-            System.out.println(playerNameX + " will place the X's");  // Output user input
+    public void scanPlayerName(boolean isPlayerX) {
+        try (Scanner playerNameScanner = new Scanner(System.in)) {
+            if (isPlayerX) {
+                System.out.println("Player 1 enter name"); // Tell user what to input
+                playerName[0] = playerNameScanner.nextLine();  // Read user input
+                System.out.println(playerName[0] + " will place the X's");  // Display user input
+            } else {
+                System.out.println("Player 2 enter name");
+                playerName[1] = playerNameScanner.nextLine();
+                System.out.println(playerName[1] + " will place the O's");
+            }
         }
     }
 
-    public void setPlayerNameY() {
-        try (Scanner playerNameScannerO = new Scanner(System.in)) {
-            System.out.println("Player 2 enter name");
-
-            String playerNameO = playerNameScannerO.nextLine();  // Read user input
-            System.out.println(playerNameO + " will place the O's");  // Output user input
+    public void playRandom() {
+        Random random = new Random(); // * random number generator
+        try {
+            this.setPiece(random.nextInt(10));
+        } catch (Exception e) {
+            this.playRandom(); // if random location is already occupied, tries again with a new random number
         }
     }
 
